12394c75d49ce5142a2e3359181edab472131862
fierew <67460036+fierew@users.noreply.github.com>
2020-08-18 13:49:13 +0800

    123

--- src/main/java/com/weigh/verification/VerificationApplication.java
+++ src/main/java/com/weigh/verification/VerificationApplication.java
@@ -3,16 +3,23 @@ package com.weigh.verification;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author xuyang
  */
 @MapperScan("com.weigh.verification.dao")
 @SpringBootApplication
+@Controller
 public class VerificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VerificationApplication.class, args);
     }
 
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
 }
