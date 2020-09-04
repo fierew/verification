908162faf82703a04e9d51ae079d354eb03be7ad
fierew <67460036+fierew@users.noreply.github.com>
2020-09-04 16:04:38 +0800

    123

--- src/main/java/com/weigh/verification/VerificationApplication.java
+++ src/main/java/com/weigh/verification/VerificationApplication.java
@@ -5,7 +5,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
