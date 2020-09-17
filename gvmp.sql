31151d797b61d53121ecd76fb3922967d922f159
xuyang <xuyang_5@163.com>
2020-09-17 16:40:56 +0800

    12

--- gvmp.sql
+++ gvmp.sql
@@ -62,33 +62,33 @@ CREATE TABLE `template` (
 
 
 CREATE TABLE `rbac_dept` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `parent_id` int NOT NULL DEFAULT '0' COMMENT '父级ID',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '机构名称',
-  `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
-  `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
-  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+ `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
+ `parent_id` int NOT NULL DEFAULT '0' COMMENT '父级ID',
+ `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '机构名称',
+ `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
+ `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
+ `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+ `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+ `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 CREATE TABLE `rbac_resource` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `parent_id` int NOT NULL DEFAULT '0' COMMENT '父级ID',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '机构名称',
-  `type` tinyint NOT NULL DEFAULT '0' COMMENT '类型，0:菜单，1：按钮，2：接口',
-  `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '图标，type等于0时存在',
-  `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '标识，type等于1或2时存在',
-  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '标识，type等于0时存在',
-  `is_hide` tinyint NOT NULL DEFAULT '0' COMMENT '是否隐藏，0不隐藏，1隐藏，type等于0时有用',
-  `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `state` tinyint NOT NULL DEFAULT '1' COMMENT '状态：1启用，0禁用',
-  `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
-  `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
-  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+ `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
+ `parent_id` int NOT NULL DEFAULT '0' COMMENT '父级ID',
+ `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '机构名称',
+ `type` tinyint NOT NULL DEFAULT '0' COMMENT '类型，0:菜单，1：按钮，2：接口',
+ `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '图标，type等于0时存在',
+ `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '标识，type等于1或2时存在',
+ `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '标识，type等于0时存在',
+ `is_hide` tinyint NOT NULL DEFAULT '0' COMMENT '是否隐藏，0不隐藏，1隐藏，type等于0时有用',
+ `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
+ `state` tinyint NOT NULL DEFAULT '1' COMMENT '状态：1启用，0禁用',
+ `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
+ `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+ `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+ `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -114,34 +114,36 @@ CREATE TABLE `rbac_role_resource` (
 
 
 CREATE TABLE `rbac_role` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '角色名称',
-  `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
-  `data_range` tinyint NOT NULL DEFAULT '0' COMMENT '数据范围，0:仅允许查看自己，1：仅允许查看本部门，2：允许查看本部门及下属部门，3：自定义',
-  `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
-  `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
-  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+ `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
+ `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '角色名称',
+ `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
+ `data_range` tinyint NOT NULL DEFAULT '0' COMMENT '数据范围，0:仅允许查看自己，1：仅允许查看本部门，2：允许查看本部门及下属部门，3：自定义',
+ `dept_ids` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '机构ID',
+ `resource_ids` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '资源ID',
+ `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
+ `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+ `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+ `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 
 CREATE TABLE `rbac_user` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `dept_id` int NOT NULL DEFAULT '0' COMMENT '机构ID',
-  `role_id` int NOT NULL DEFAULT '0' COMMENT '角色ID',
-  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '密码',
-  `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '真实姓名',
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '邮箱',
-  `mobile` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机号',
-  `sex` tinyint NOT NULL DEFAULT '0' COMMENT '性别：0未知，1男，2女',
-  `age` int NOT NULL DEFAULT '0' COMMENT '年龄',
-  `login_num` int NOT NULL DEFAULT '0' COMMENT '登录次数',
-  `state` tinyint NOT NULL DEFAULT '1' COMMENT '状态：1启用，0禁用',
-  `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
-  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+ `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
+ `dept_id` int NOT NULL DEFAULT '0' COMMENT '机构ID',
+ `role_id` int NOT NULL DEFAULT '0' COMMENT '角色ID',
+ `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '密码',
+ `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '真实姓名',
+ `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+ `mobile` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机号',
+ `sex` tinyint NOT NULL DEFAULT '0' COMMENT '性别：0未知，1男，2女',
+ `age` int NOT NULL DEFAULT '0' COMMENT '年龄',
+ `login_num` int NOT NULL DEFAULT '0' COMMENT '登录次数',
+ `state` tinyint NOT NULL DEFAULT '1' COMMENT '状态：1启用，0禁用',
+ `is_delete` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除',
+ `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+ `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
