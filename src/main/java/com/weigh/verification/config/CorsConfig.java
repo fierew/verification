75518b7c9ef0eb202faf1b75d55fddcc176fccd1
徐洋 <xuyang@xuyangdeMacBook-Pro.local>
2020-11-15 14:44:05 +0800

    123

--- src/main/java/com/weigh/verification/config/CorsConfig.java
+++ src/main/java/com/weigh/verification/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8000")
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
